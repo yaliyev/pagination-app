b2b8b99a567f90e331e94aa6ef20bb15379ab31d
YaqubAliyev4513 <tu6w6s3r7@code.edu.az>
2023-11-26 19:09:47 +0400

    detailed modal beer

--- pagination-app-react/src/components/BeerTable.jsx
+++ pagination-app-react/src/components/BeerTable.jsx
@@ -1,17 +1,18 @@
-import React from 'react'
-import { Space, Table, Tag,Button } from 'antd';
-const BeerTable = ({beers}) => {
-
+import React, { useState } from 'react'
+import { Space, Table, Tag, Button, Modal } from 'antd';
+const BeerTable = ({ beers }) => {
+    const [isDetailedBeerModalOpen, setIsDetailedBeerModalOpen] = useState(false);
+    const [currentBeer,setCurrentBeer] = useState(null);
     const columns = [
         {
-          title: 'ID',
-          dataIndex: 'id',
-          key: 'id'
+            title: 'ID',
+            dataIndex: 'id',
+            key: 'id'
         },
         {
-          title: 'Name',
-          dataIndex: 'name',
-          key: 'beername',
+            title: 'Name',
+            dataIndex: 'name',
+            key: 'beername',
         },
         {
             title: 'First Brewed',
@@ -23,24 +24,51 @@ const BeerTable = ({beers}) => {
             key: 'key',
             dataIndex: 'key',
             render: (text, record) => (
-             <Button type="primary">
-               {"Info"}
-             </Button>
+                <>
+                    <Button type="primary" onClick={() => { setIsDetailedBeerModalOpen(true);setCurrentBeer(record);}}>
+                        {"Info"}
+                    </Button>
+
+                </>
+
             ),
         }
-      ];
+    ];
 
-      
 
-      const data = beers.map((beer) => ({
+
+    const data = beers.map((beer) => ({
         ...beer,
-        key: beer.id, 
-      }));
-      
+        key: beer.id,
+    }));
 
-  return (
-    <Table columns={columns} dataSource={data} />
-  )
+
+    return (
+        <>
+            <Modal title="Detail Beer Info Modal" onCancel={()=>{setIsDetailedBeerModalOpen(false)}} open={isDetailedBeerModalOpen}
+            footer={[
+                <Button key="back" onClick={() => { setIsDetailedBeerModalOpen(false) }}>
+                  Return
+                </Button>
+                
+              ]}>
+                {
+                   currentBeer? 
+                   <>
+                   <div><h3>Name:</h3> {currentBeer.name}</div>
+                   <div><h3>Description:</h3> {currentBeer.description}</div>
+                   <div><h3>Attenuation level:</h3> {currentBeer.attenuation_level} </div>
+                   
+                   </> :
+                   <></> 
+                }
+                
+                
+            </Modal>
+            <Table columns={columns} dataSource={data} />
+        </>
+
+    )
 }
 
 export default BeerTable
