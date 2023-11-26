963aacd0892be222feaf13254494956b89c41f5c
YaqubAliyev4513 <tu6w6s3r7@code.edu.az>
2023-11-27 03:54:33 +0400

    search beer card added

--- pagination-app-react/src/App.jsx
+++ pagination-app-react/src/App.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect } from 'react'
 import { useState } from 'react'
-import { getBeers, getBeersByPageAndByPerPage } from './api/punk_request'
+import { getBeers, getBeersByPageAndByPerPage,searchBeersByName } from './api/punk_request'
 import BeerTable from './components/BeerTable';
 import BeerCard from './components/BeerCard';
-import { Col, Row, Pagination, Select,Modal,Button } from 'antd';
+import { Col, Row, Pagination, Select, Modal, Button, Input } from 'antd';
+import Slider from '@mui/material/Slider';
 
 
 const itemsPerPageOptions = [10, 15, 20, 25, 50, 80];
@@ -17,6 +18,7 @@ function App() {
 
   const [isDetailedBeerCardModalOpen, setIsDetailedBeerCardModalOpen] = useState(false);
   const [currentBeerCard, setCurrentBeerCard] = useState(null);
+  const [searchBeerCardInput,setSearchBeerCardInput] = useState("");
 
   useEffect(() => {
     async function loadData() {
@@ -36,39 +38,61 @@ function App() {
 
   }, [currentPaginationPage, itemsPerPage]) // Ikili deyishiklik gozleyen dependency array
 
+  useEffect(()=>{
+    async function loadDataBySearch() {
+      const data = await searchBeersByName(searchBeerCardInput);
+      setPaginationBeers(data.slice(0,itemsPerPage));
+    }
+    loadDataBySearch();
+
+  },[searchBeerCardInput])
+
   return (
     <>
       <BeerTable beers={beers} />
 
 
       <Row>
+        <Col style={{ marginLeft: '20px' }} span={6}>
+          <Input onChange={(e)=>{setSearchBeerCardInput(e.target.value)}} placeholder="Search beer card:" />
 
-      <Modal title="Detail Beer Card Info Modal" onCancel={() => { setIsDetailedBeerCardModalOpen(false) }} open={isDetailedBeerCardModalOpen}
-        footer={[
-          <Button key="back" onClick={() => { setIsDetailedBeerCardModalOpen(false) }}>
-            Return
-          </Button>
+        </Col>
+        <Col style={{ marginLeft: '20px' }}  span={6}>
+         
+        <Slider  defaultValue={50}  step={1}  marks={[{value:0,label:"0"},{value:100,label:"100"}]} />
+        </Col>
+        <Col style={{ marginLeft: '20px' }}  span={6}>
+          <Button type='primary'>Submit</Button>
+        </Col>
+      </Row>
+      <Row>
 
-        ]}>
-        {
-          currentBeerCard ?
-            <>
-              
-              <div><h3>Description:</h3> {currentBeerCard.description}</div>
-              <div><h3>Food Pairing:</h3> {currentBeerCard.food_pairing}</div>
-              <div><h3>Tag line:</h3> {currentBeerCard.tagline} </div>
+        <Modal title="Detail Beer Card Info Modal" onCancel={() => { setIsDetailedBeerCardModalOpen(false) }} open={isDetailedBeerCardModalOpen}
+          footer={[
+            <Button key="back" onClick={() => { setIsDetailedBeerCardModalOpen(false) }}>
+              Return
+            </Button>
 
-            </> :
-            <></>
-        }
+          ]}>
+          {
+            currentBeerCard ?
+              <>
+
+                <div><h3>Description:</h3> {currentBeerCard.description}</div>
+                <div><h3>Food Pairing:</h3> {currentBeerCard.food_pairing}</div>
+                <div><h3>Tag line:</h3> {currentBeerCard.tagline} </div>
+
+              </> :
+              <></>
+          }
 
 
-      </Modal>
+        </Modal>
 
         {
           paginationBeers.map((beer, index) => {
             return <Col key={index} span={6}>
-              <BeerCard  beer={beer} setCurrentBeerCard={setCurrentBeerCard} setIsDetailedBeerCardModalOpen={setIsDetailedBeerCardModalOpen} />
+              <BeerCard beer={beer} setCurrentBeerCard={setCurrentBeerCard} setIsDetailedBeerCardModalOpen={setIsDetailedBeerCardModalOpen} />
             </Col>
           })
         }
--- pagination-app-react/src/api/punk_request.js
+++ pagination-app-react/src/api/punk_request.js
@@ -9,4 +9,11 @@ async function getBeers(){
 async function getBeersByPageAndByPerPage(pageNumber,perPageQuantity){
     return await axios.get(API_URL+`?page=${pageNumber}&per_page=${perPageQuantity}`).then(response=>response.data);
 }
-export  {getBeers,getBeersByPageAndByPerPage}; 
+async function searchBeersByName(searchParam){
+    let resultAPI_URL = API_URL;
+    if(searchParam.trim().length > 0){
+        resultAPI_URL = API_URL+`?beer_name=${searchParam}`;
+    }
+    return await axios.get(resultAPI_URL).then(response=>response.data);
+}
+export  {getBeers,getBeersByPageAndByPerPage,searchBeersByName}; 
